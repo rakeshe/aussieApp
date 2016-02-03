209096149de9527a358d56408b95e1934b6fdbee
rakeshe <Rakesh.Shrestha@hotelclub.com>
2016-02-04 07:51:54 +1100

    include phpunit test cases

--- Test/Unit/App/BaseControllerTest.php
+++ Test/Unit/App/BaseControllerTest.php
@@ -2,4 +2,9 @@
 class App_BaseControllerTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testCreateRouterSuccessfully(){
+        $router = new App_RouterTest();
+        $this->assertInstanceOf('App\Router', $router);
+    }
+
 }
